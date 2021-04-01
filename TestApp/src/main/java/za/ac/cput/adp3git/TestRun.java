c2780e6499577ccb9e5d2ae6057a099494e454ac
SergioFisher <81588876+SergioFisher@users.noreply.github.com>
2021-04-01 21:26:26 +0200

    Add files via upload

---
+++ TestApp/src/main/java/za/ac/cput/adp3git/TestRun.java
@@ -0,0 +1,43 @@
+package za.ac.cput.adp3git;
+
+public class TestRun {
+
+
+        private String a;
+        private String b;
+
+        public TestRun(){}
+
+        public TestRun(String a, String b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public String getA()
+        {
+            return a;
+        }
+
+        public void setA(String a)
+        {
+            this.a = a;
+        }
+
+        public String getB()
+        {
+            return b;
+        }
+
+        public void setB(String b)
+        {
+            this.b = b;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "Test{" + "a='" + a + '\'' + ", b='" + b + '\'' + '}';
+        }
+}
+
